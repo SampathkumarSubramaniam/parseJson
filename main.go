ee540e4a24af94c05b4d410485003908fc925b3c
sampathkumar subramaniam <sampathkumar.subramaniam@gmail.com>
2025-02-15 22:21:16 +0100

    new commits

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type SkillSet struct {
+	LanguagesKnown []string
+	Passion        []string
+}
+
+type UserData struct {
+	UserId         int
+	UserName       string
+	IsActive       bool
+	Roles          []string
+	TrainingCredit float32 `json:"training-credit"` // json file contains different key(training-credit), hence we map to TrainingCredit
+	Skills         SkillSet
+}
+
+func main() {
+	var userInfo []UserData
+	fileByte, err := os.ReadFile("resources/test.json")
+	if err != nil {
+		fmt.Printf("Error occured while reading hr json. Err:%s", err)
+	}
+	err = json.Unmarshal(fileByte, &userInfo)
+	if err != nil {
+		fmt.Printf("Error occured while unmarshalling hr json. Err:%s", err)
+	}
+	// for loop to iterate over the records
+	for _, value := range userInfo {
+		fmt.Printf("------Employee Details: %s------\n", value.UserName)
+		fmt.Println("User ID:", value.UserId)
+		fmt.Println("Is Active:", value.IsActive)
+		fmt.Println("Available training credit", value.TrainingCredit)
+		fmt.Println("Languages known:", value.Skills.LanguagesKnown)
+		fmt.Println("Passionate about:", value.Skills.Passion)
+	}
+}
